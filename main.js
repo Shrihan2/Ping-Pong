a8553d1e5fb021875a2c93c76a3fe9db7e71e5e9
Shrihan2 <90690673+Shrihan2@users.noreply.github.com>
2023-08-02 16:56:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,6 +16,9 @@ var ball = {
 }
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent("canvas");
+  video=createCapture(VIDEO);
+  video.size(700,600);
 }
 
 function restart(){}
